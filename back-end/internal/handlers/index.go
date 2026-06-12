bcf3755595abfe5e2006f65ea8adf4885db20cda
HaxTwoWater <louis.jeannot@ynov.com>
2026-06-12 15:35:55 +0200

    feat(handlers): added the first handler for index gonna add later on everything that need to be handled

---
+++ back-end/internal/handlers/index.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"net/http"
+
+	"groupie_tracker/internal/render"
+)
+
+func Index(v *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			v.Render(w, "error.html", map[string]any{
+				"Code":    404,
+				"Message": "Page introuvable",
+			})
+			return
+		}
+		v.Render(w, "index.html", map[string]any{})
+	}
+}
