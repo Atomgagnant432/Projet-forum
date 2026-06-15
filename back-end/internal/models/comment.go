c24c438ca1348bacde2b2846a7702e52a7fde567
HaxTwoWater <louis.jeannot@ynov.com>
2026-06-15 12:02:26 +0200

    feat(models): added the struc and the function for sql querry for the comment model

---
+++ back-end/internal/models/comment.go
@@ -0,0 +1,51 @@
+package models
+
+type Comment struct {
+	ID        int
+	UserID    int
+	Avatar    string
+	Pseudo     string
+	Content   string
+	CreatedAt string
+}
+
+func GetCommentsByPostID(db *sql.DB, postID int) ([]Comment, error) {
+	rows, err := db.Query(`
+		SELECT
+			c.id,
+			c.user_id,
+			u.pseudo,
+			c.content,
+			c.created_at
+		FROM comments c
+		JOIN users u ON u.id = c.user_id
+		WHERE c.post_id = ?
+		ORDER BY c.created_at ASC
+	`, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var comments []Comment
+
+	for rows.Next() {
+		var comment Comment
+
+		err := rows.Scan(
+			&comment.ID,
+			&comment.UserID,
+			&comment.Pseudo,
+			&comment.Content,
+			&comment.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		comment.Avatar = "👤"
+		comments = append(comments, comment)
+	}
+
+	return comments, rows.Err()
+}
