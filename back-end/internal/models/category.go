9d6576cedc74207bb00ffddfbd5d4f2cf70d88e4
HaxTwoWater <louis.jeannot@ynov.com>
2026-06-15 12:00:18 +0200

    feat(models): added the the struct for the category model along the get categories for sql purposes

--- back-end/internal/models/category.go
+++ back-end/internal/models/category.go
@@ -4,3 +4,29 @@ type Category struct {
 	ID   int
 	Name string
 }
+
+func GetCategories(db *sql.DB) ([]Category, error) {
+	rows, err := db.Query(`
+		SELECT id, name
+		FROM category
+		ORDER BY name ASC
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []Category
+
+	for rows.Next() {
+		var category Category
+
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+
+		categories = append(categories, category)
+	}
+
+	return categories, rows.Err()
+}
