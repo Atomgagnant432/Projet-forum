2f78a59e522de0081e50bd9f9819e5d4dc6ef7d2
HaxTwoWater <louis.jeannot@ynov.com>
2026-06-15 12:10:59 +0200

    feat(models): added the struct and function for sql querry for the user model

---
+++ back-end/internal/models/user.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"database/_sql"
+)
+
+
+type User struct {
+	ID        int
+	Pseudo    string
+	Email     string
+	Avatar    string
+	CreatedAt string
+}
+
+
+func GetUserByID(db *sql.DB, id int) (*User, error) {
+	query := `
+		SELECT
+			id,
+			pseudo,
+			email,
+			avatar,
+			created_at
+		FROM users
+		WHERE id = ?
+	`
+
+	var user User
+
+	err := db.QueryRow(query, id).Scan(
+		&user.ID,
+		&user.Pseudo,
+		&user.Email,
+		&user.Avatar,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+
+	return &user, nil
+}
