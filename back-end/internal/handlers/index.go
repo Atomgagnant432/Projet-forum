4a618d5b0b4cbe739179adb0b2d9777b78b4f9d4
DECUNG Tessa <tessa.decung@ynov.com>
2026-06-16 14:06:02 +0200

    fix(handlers): passed current user id to GetHomePosts

--- back-end/internal/handlers/index.go
+++ back-end/internal/handlers/index.go
@@ -27,7 +27,9 @@ func Index(db *sql.DB, v *render.Render) http.HandlerFunc {
 			return
 		}
 
-		posts, err := models.GetHomePosts(db)
+		currentUserID := 1
+
+		posts, err := models.GetHomePosts(db, currentUserID)
 		if err != nil {
 			http.Error(w, "Erreur chargement posts", http.StatusInternalServerError)
 			return
